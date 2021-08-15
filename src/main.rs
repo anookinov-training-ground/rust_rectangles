aa12024249fc497af43c14fef4af1ce86289f3af
Alexander Nookinov <anookinov@gmail.com>
2021-08-15 19:19:03 +0700

    add associate functions and methods

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,25 @@ struct Rectangle {
     height: u32,
 }
 
+impl Rectangle {
+    // method
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+
+    // associated function
+    fn square(size: u32) -> Rectangle {
+        Rectangle {
+            width: size,
+            height: size,
+        }
+    }
+}
+
 fn main() {
     let rect1 = Rectangle {
         width: 30,
@@ -17,6 +36,36 @@ fn main() {
 
     println!("rect1 is {:?}", rect1);
     println!("rect1 is {:#?}", rect1);
+
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        rect1.area()
+    );
+
+    // automatic referencing and dereferencing
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        &rect1.area()
+    );
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        (&rect1).area()
+    );
+
+    let rect2 = Rectangle {
+        width: 10,
+        height: 40,
+    };
+    let rect3 = Rectangle {
+        width: 60,
+        height: 45,
+    };
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
+
+    // calling associated function within Rectangle struct's namespace
+    let sq = Rectangle::square(3);
+    println!("{:#?}", sq)
 }
 
 fn area(rectangle: &Rectangle) -> u32 { // rectangle borrows the rect1 struct rather than takes ownership of it.
